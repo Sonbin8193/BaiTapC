edb3be1d55040218f734c6129a362b41be0a3fd2
Sơn Bin <sonbin8193@gmail.com>
2024-03-29 00:58:39 +0700

    Baitap ngày 28/3

---
+++ Bai7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Bai7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Nhập một ký tự: ");
+            char kyTu = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            Console.WriteLine($"Mã Unicode của ký tự '{kyTu}' là {(int)kyTu}");
+            Console.Write("Nhập mã Unicode từ 0 đến 255: ");
+            int maUnicode = int.Parse(Console.ReadLine());
+            if (maUnicode <256 && maUnicode >-1)
+            {
+                char kyTuUnicode = (char)maUnicode;
+                Console.WriteLine($"Ký tự của mã Unicode {maUnicode} là '{kyTuUnicode}'");
+            }
+            else
+            {
+                Console.WriteLine("Mã Unicode không hợp lệ.");
+            }
+        }
+    }
+}
