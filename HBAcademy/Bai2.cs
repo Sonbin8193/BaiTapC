b4497b1361de15108268ec1e69537394da8bd3cd
Sonbin8193 <126962131+Sonbin8193@users.noreply.github.com>
2024-03-29 01:10:57 +0700

    Add files via upload
    Bai Tap so 2

---
+++ HBAcademy/Bai2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Test1
+    {
+        internal static void Hello()
+        {
+            Console.WriteLine("How are you my friend");
+        }
+    }
+    namespace Bai2Child
+    {
+        internal class Test2
+        {
+            internal static void Hello()
+            {
+                Console.WriteLine("I am fine, thanks");
+            }
+        }
+
+        internal class Retangle
+        {
+            public static int GanGiaTriHinhChuNhat(int a,int b)
+            {
+                int chieuDai=a;
+                int chieuRong=b;
+                Console.WriteLine("Chieu dai hinh chu nhat la " + chieuDai);
+                Console.WriteLine("Chieu rong hinh chu nhat la " + chieuRong);
+            }
+        }
+    }
+
+}
+
---
+++ HBAcademy/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HBacademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HBAcademy");
+            Bai2.Test1.Hello();
+            Bai2.Bai2Child.Test2.Hello();
+            Bai2.Bai2Child.Retangle.GanGiaTriHinhChuNhat(10,8);
+        }
+    }
+}
