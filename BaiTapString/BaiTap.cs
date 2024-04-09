989804e54b2a21fb7042a0f473cc7394a4f6f7d9
Sonbin8193 <sonbin8193@gmail.com>
2024-04-09 18:23:03 +0700

    Baitap String1

---
+++ BaiTapString/BaiTap.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace BaiTapString
+{
+    internal class BaiTap
+    {
+        public static void NhapChuoi()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Nhập một chuỗi bất kỳ: ");
+            string chuoiNhapVao = Console.ReadLine();
+
+            KiemTraKyTuChuoi(chuoiNhapVao);
+
+            static void KiemTraKyTuChuoi(string stringArray)
+            {
+                string chuoiSo = "0123456789";
+                string chuoiChuThuong = "abcdefghiklmnopqrstuvwxyz";
+                string chuoiChuHoa = chuoiChuThuong.ToUpper();
+                foreach (var item in stringArray)
+                {
+                    bool kiemTra = chuoiSo.Contains(item);
+                    if (kiemTra)
+                    {
+                        Console.WriteLine($"Chuỗi có chứa ký tự số");
+                        break;
+                    }
+                }       
+            }
+        }
+    }
+}
