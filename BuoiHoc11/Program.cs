b3126fb9d370fe217887b07497de7a7500bc95ce
Sonbin8193 <sonbin8193@gmail.com>
2024-04-19 16:56:59 +0700

    baitap buoi 11

--- BuoiHoc11/Program.cs
+++ BuoiHoc11/Program.cs
@@ -1,12 +1,54 @@
-﻿namespace BuoiHoc11
-{
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace BuoiHoc11
+{   
     internal class Program
     {   //Tạo danh sách lớp và sắp xếp theo các phương pháp sắp xếp đã học
         static void Main(string[] args)
         {
-            string[] danhSachLop = new string[]{"Lê Trung Kiên","Phạm Giang Sơn","Phí Đức Anh","Trần Chí Hào","Lê Trương Trí","Trịnh Bùi Hoàng Anh","Vũ Phụng Hoàng","Nguyễn Văn Tuấn","Cao Việt Hoàng","Phạm Minh Thành","Phạm Nguyễn Minh Phúc","Nguyễn Thiên Lộc","Nghiêm Minh Toàn"
-            };
+            Console.OutputEncoding = Encoding.UTF8;
+            List<string> danhSachLop = new List<string>();
+        nhaplai:
+            Console.WriteLine("Nhập tên các thành viên trong lớp(ấn 'end' để kết thúc): ");
+            string tenThanhVien = Console.ReadLine();
+            Console.Clear();
+            if (tenThanhVien != "end")
+            {
+                danhSachLop.Add(tenThanhVien);
+                goto nhaplai;
+            }
+            else
+            {
+                Console.WriteLine("Danh sách lớp sau khi đã sắp xếp: ");
+                string[] aray_danhSachLop = danhSachLop.ToArray(); 
+                SapXepChen(aray_danhSachLop);
+            }
+        }
+        public static void SapXepChen(string[] name)
+            {
 
+            for (int i = 0; i < name.Length - 1; i++)
+            {
+                string boNhoTen = name[i];
+                int viTri = i - 1;
+                string[] array_boNhoTen = boNhoTen.Split(" ");
+
+                while (viTri >= 0)
+                {
+                    string[] array_name = name[viTri].Split(" ");
+                    if (array_name[array_name.Length-1].CompareTo(array_boNhoTen[array_boNhoTen.Length - 1])>0 || array_name[array_name.Length - 1].CompareTo(array_boNhoTen[array_boNhoTen.Length - 1]) == 0 && array_name[array_name.Length - 2].CompareTo(array_boNhoTen[array_boNhoTen.Length - 2]) > 0)
+                    {
+                        name[viTri + 1] = name[viTri];
+                        viTri--;
+                    }
+                    else
+                    {
+                        break;
+                    }          
+                }
+                name[viTri + 1] = boNhoTen;
+            }
         }
     }
 }
--- BuoiHoc11/obj/Debug/net8.0/BuoiHoc11.AssemblyInfo.cs
+++ BuoiHoc11/obj/Debug/net8.0/BuoiHoc11.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuoiHoc11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ccf3baec32077820cbe66e0348f347fdb52e1ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9beeb8a714c9021249037200b734272c81537c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuoiHoc11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuoiHoc11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
