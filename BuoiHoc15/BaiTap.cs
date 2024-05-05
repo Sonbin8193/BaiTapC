ed8293c612b423485e9b8167e77afec1d5eb2fd1
Sonbin8193 <126962131+Sonbin8193@users.noreply.github.com>
2024-05-05 19:51:21 +0700

    buoihoc15

---
+++ BuoiHoc15/BaiTap.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuoiHoc15
+{
+    public interface ActionRest
+    {
+        void Rest();
+    }
+    public interface Action
+    {
+        void Run();
+    }
+    public abstract class Animals : Action,ActionRest
+    {
+        string name;
+        int age;
+        string food;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Food { get => food; set => food = value; }
+
+        abstract public void Run();
+        public abstract void Rest();
+    }
+
+    public class Cat : Animals
+    {
+        public override void Rest()
+        {
+            Console.WriteLine("Mèo đang nghỉ");
+        }
+        public override void Run()
+        {
+            Console.WriteLine("Mèo đang chạy");
+        }
+    }
+    public class Dog : Animals
+    {
+        public override void Rest()
+        {
+            Console.WriteLine("Chó đang nghỉ");
+        }
+        public override void Run()
+        {
+            Console.WriteLine("Chó đang chạy");
+        }
+    }
+
+    public class People : Action
+    {
+        string name;
+        int old;
+        string address;
+        string mobilePhone;
+
+        public string Name { get => name; set => name = value; }
+        public int Old { get => old; set => old = value; }
+        public string Address { get => address; set => address = value; }
+        public string MobilePhone { get => mobilePhone; set => mobilePhone = value; }
+
+        public void Run()
+        {
+            Console.WriteLine("Bố mày đang chạy");
+        }
+    }
+    public class MyCar : Action,ActionRest
+    {
+        string name;
+        int old;
+        string color;
+
+        public string Name { get => name; set => name = value; }
+        public int Old { get => old; set => old = value; }
+        public string Color { get => color; set => color = value; }
+
+        public void Rest()
+        {
+            Console.WriteLine("Xe đang đỗ");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Xe đang chạy");
+        }
+    }
+}
---
+++ BuoiHoc15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace BuoiHoc15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.OutputEncoding = Encoding.UTF8;
+           Cat meoDen = new Cat
+            {
+                Name = "Mèo đen",
+                Age = 10,
+                Food = "Cá khô"
+            };
+            Dog choTrang = new Dog
+            {
+                Name = "Phốc",
+                Age = 10,
+                Food = "Cơm"
+            };
+            People person = new People
+            {
+                Name = "Sơn",
+                Old = 29,
+                Address = "Hà Nội",
+                MobilePhone = "0962932193"
+            };
+            MyCar toyota = new MyCar
+            {
+                Name = "Huyndai",
+                Color = "Xanh",
+                Old = 6
+            };
+            meoDen.Run();
+        }
+    }
+}
